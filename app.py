f934fc9faaf1767df5a9a9eed877d1ac4d390e6a
Brian Aston <63326484+astonbdev@users.noreply.github.com>
2024-05-15 09:48:49 -0700

    Update app.py
    Removing `/users/` auth check to test render deploy and dbcentral

--- app.py
+++ app.py
@@ -151,10 +151,6 @@ def list_users():
     Can take a 'q' param in querystring to search by that username.
     """
 
-    if not g.user:
-        flash("Access unauthorized.", "danger")
-        return redirect("/")
-
     search = request.args.get('q')
 
     if not search:
